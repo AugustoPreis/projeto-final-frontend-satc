94101c830d1edb379ef0b00202bd21c139ac86dd
Augusto Tomasi <augustopreisthomasi@gmail.com>
2023-11-08 17:48:18 -0300

    adiciona config provider do antd

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { ConfigProvider } from 'antd';
+import ptBR from 'antd/lib/locale/pt_BR';
 import AppRouter from './router/AppRouter';
 
 const root = createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <AppRouter />
+    <ConfigProvider locale={ptBR}>
+      <AppRouter />
+    </ConfigProvider>
   </React.StrictMode>,
 );
