e3fa9ed4d364dee097ebd39db32ce198b4cd5fd5
Augusto Tomasi <augustopreisthomasi@gmail.com>
2023-11-08 16:56:17 -0300

    remove funcao de login

--- frontend/src/login/Login.jsx
+++ frontend/src/login/Login.jsx
@@ -1,16 +1,10 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 import { Button, Col, Form, Input, Row } from 'antd';
 import { UnlockOutlined, UserOutlined } from '@ant-design/icons';
 import './login.css';
 
 export default function Login() {
   const [form] = Form.useForm();
-  const navigate = useNavigate();
-
-  const handleSubmit = (user) => {
-    navigate('/dashboard');
-  }
 
   return (
     <Row justify='center'
@@ -22,8 +16,7 @@ export default function Login() {
         sm={14}
         xs={24}>
         <Form form={form}
-          layout='vertical'
-          onFinish={handleSubmit}>
+          layout='vertical'>
           <Row gutter={[10, 0]}
             justify='center'
             className='login-card'>
