6468923be9aae8489d80eff79b0869a0963f4a76
Augusto Tomasi <augustopreisthomasi@gmail.com>
2023-11-08 13:38:20 -0300

    cria tela de login

--- frontend/src/login/Login.jsx
+++ frontend/src/login/Login.jsx
@@ -1,8 +1,78 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Button, Col, Form, Input, Row } from 'antd';
+import { UnlockOutlined, UserOutlined } from '@ant-design/icons';
+import auth from '../utils/auth';
+import './login.css';
 
 export default function Login() {
+  const [form] = Form.useForm();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (auth.isAuthenticated()) {
+      navigate('/dashboard');
+    }
+  }, []);
+
+  const handleSubmit = (user) => {
+    auth.login(user);
+    navigate('/dashboard');
+  }
+
   return (
-    <div>
-    </div>
+    <Row justify='center'
+      align='middle'
+      className='login'>
+      <Col xl={7}
+        lg={8}
+        md={10}
+        sm={14}
+        xs={24}>
+        <Form form={form}
+          layout='vertical'
+          onFinish={handleSubmit}>
+          <Row gutter={[10, 0]}
+            justify='center'
+            className='login-card'>
+            <Col span={24}
+              className='login-title'>
+              MSystem
+            </Col>
+            <Col span={24}
+              className='login-subtitle'>
+              Informe seus dados no formulário abaixo para acessar o sistema.
+            </Col>
+            <Col span={24}>
+              <Form.Item name='login'
+                label='Login'
+                rules={[{ required: true, message: 'Informe o login' }]}>
+                <Input placeholder='Informe o login'
+                  prefix={<UserOutlined />} />
+              </Form.Item>
+            </Col>
+            <Col span={24}>
+              <Form.Item name='senha'
+                label='Senha'
+                rules={[{ required: true, message: 'Informe a senha' }]}>
+                <Input.Password placeholder='Informe a senha'
+                  prefix={<UnlockOutlined />} />
+              </Form.Item>
+            </Col>
+            <Col xl={8}
+              lg={9}
+              md={8}
+              sm={7}
+              xs={24}>
+              <Button block
+                type='primary'
+                onClick={form.submit}>
+                Entrar
+              </Button>
+            </Col>
+          </Row>
+        </Form>
+      </Col>
+    </Row>
   );
 }
